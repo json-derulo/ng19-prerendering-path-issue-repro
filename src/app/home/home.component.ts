8e9142ba32d385540692233ad709442f5c599ae8
Daniel Kimmich <json-derulo@users.noreply.github.com>
2024-11-03 21:56:11 +0100

    update to angular 19

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,6 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-home',
     imports: [],
-    standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
